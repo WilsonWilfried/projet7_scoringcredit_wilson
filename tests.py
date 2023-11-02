4ec991c0d47be24803f262904e979479f8b48129
Wilson Wilfried <lilwil004@gmail.com>
2023-11-03 00:48:04 +0100

    modication du fichier tests et ajouts des autres fichiers

--- tests.py
+++ tests.py
@@ -8,12 +8,12 @@ def data():
     return pd.read_csv("data.csv", index_col=[0])
 
 @pytest.fixture(scope='module')
-def data():
+def data_train():
     """Get customers data_train to feed into the tests"""
     return pd.read_csv("application_train.csv", index_col=[0])
 
 @pytest.fixture(scope='module')
-def data():
+def data_test():
     """Get customers data_test to feed into the tests"""
     return pd.read_csv("application_test.csv", index_col=[0])
 
@@ -26,11 +26,11 @@ def test_data_duplicates(data):
 
 def test_data_target_col(data):
     """Test that the train dataframe has a 'target' column"""
-    assert 'TARGET' in data.columns
+    assert 'TARGET' in data.columns.tolist()
 
 
-def test_train_test_sizes(application_train, application_test):
+def test_train_test_sizes(data_train, data_test):
     """verifions si le data d'entrainement et le data de production ont le même nombre de colonnes, sans le target"""
-    train_size = application_train.drop(columns='TARGET').shape[1]
-    test_size = application_test.shape[1]
+    train_size = data_train.drop(columns='TARGET').shape[1]
+    test_size = data_test.shape[1]
     assert train_size == test_size
