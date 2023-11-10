a41c131e2c6029cf34ac374579185890639b22fb
WilsonWilfried <144227776+WilsonWilfried@users.noreply.github.com>
2023-11-10 16:13:48 +0100

    Update dash.py

--- dash.py
+++ dash.py
@@ -1,20 +1,7 @@
-# import packages
-# ------------------------------------
-import requests
-import json
-import joblib
-from pandas import json_normalize
-import pandas as pd
-import plotly.graph_objects as go
 import streamlit as st
-import seaborn as sns
-import shap
-from shap.plots import waterfall
-import matplotlib.pyplot as plt
-from PIL import Image
-import streamlit.components.v1 as components
-
-def main():
-    print('Hello')
-        
-main()
+import pandas as pd
+ 
+st.write("""
+# My first app
+Hello *world!*
+""")
